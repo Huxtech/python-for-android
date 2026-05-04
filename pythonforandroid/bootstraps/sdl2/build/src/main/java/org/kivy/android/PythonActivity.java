ae52a1b0b8b8465ef323f5c41575cf211bfdcf4c
Huxtech <101998889+Huxtech@users.noreply.github.com>
2026-05-04 19:43:18 +0100

    Reduce loading screen removal timer from 10s to 7s

--- pythonforandroid/bootstraps/sdl2/build/src/main/java/org/kivy/android/PythonActivity.java
+++ pythonforandroid/bootstraps/sdl2/build/src/main/java/org/kivy/android/PythonActivity.java
@@ -398,7 +398,7 @@ public class PythonActivity extends SDLActivity {
                                         }
                                     };
                             loadingScreenRemovalTimer = new Timer();
-                            loadingScreenRemovalTimer.schedule(removalTask, 10000);
+                            loadingScreenRemovalTimer.schedule(removalTask, 7000);
                         }
                     }
                 });
